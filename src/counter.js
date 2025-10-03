b7f8b665aed6a853fef68112ef24fec8ea7f29fd
Varshith Raj Basa <varshithraj.basa@gmail.com>
2025-10-03 22:33:47 +0530

    rewrite the counter, App

--- src/counter.js
+++ src/counter.js
@@ -1,8 +1,8 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count = 0) => {
-    element.innerHTML = `${count} times clicked..!`
+export function setupCounter() {
+  const [counter, setCounter] = useState(0);
+    return <div>
+      <button onClick={()=>{setCounter(c=>c+1)}}>click me</button>
+      {count && <p>{count} times clicked..!</p>}
+      </div>;
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
+  
